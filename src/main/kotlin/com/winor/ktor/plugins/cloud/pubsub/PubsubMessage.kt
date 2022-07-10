1f4641001d723f4ebc6a4e682030a2093e18831a
katsumata <12413150+winor30@users.noreply.github.com>
2022-07-10 17:32:07 +0900

    feat: add pubsub plugin

---
+++ src/main/kotlin/com/winor/ktor/plugins/cloud/pubsub/PubsubMessage.kt
@@ -0,0 +1,19 @@
+package com.winor.ktor.plugins.cloud.pubsub
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.*
+
+data class PubsubMessage<T>(val message: Message) {
+
+  companion object {
+    val mapper = jacksonObjectMapper()
+  }
+
+  data class Message(val messageId: String, val publishTime: String, val data: String)
+
+  inline fun <reified R : T> toDecodedData(): R {
+    val decoded = String(Base64.getDecoder().decode(this.message.data))
+    return mapper.readValue(decoded, R::class.java)
+  }
+}
+
---
+++ src/main/kotlin/com/winor/ktor/plugins/cloud/pubsub/PubsubPushMessageTransformationPlugin.kt
@@ -0,0 +1,48 @@
+package com.winor.ktor.plugins.cloud.pubsub
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.ktor.serialization.Configuration
+import io.ktor.server.application.ApplicationCallPipeline
+import io.ktor.server.application.BaseRouteScopedPlugin
+import io.ktor.server.request.ApplicationReceivePipeline
+import io.ktor.util.AttributeKey
+import io.ktor.utils.io.ByteReadChannel
+import io.ktor.utils.io.readUTF8Line
+import java.util.Base64
+
+class PubsubPushMessageTransformationPlugin<T : Any> :
+  BaseRouteScopedPlugin<Configuration, PubsubPushMessageTransformationPlugin<T>> {
+
+  companion object {
+    val mapper = jacksonObjectMapper()
+  }
+
+  override val key: AttributeKey<PubsubPushMessageTransformationPlugin<T>> =
+    AttributeKey("PubsubPushMessageTransformationPlugin")
+
+  override fun install(
+    pipeline: ApplicationCallPipeline,
+    configure: Configuration.() -> Unit
+  ): PubsubPushMessageTransformationPlugin<T> {
+    val plugin = PubsubPushMessageTransformationPlugin<T>()
+    pipeline.receivePipeline.intercept(ApplicationReceivePipeline.Before) { data ->
+      if (data !is ByteReadChannel) {
+        proceedWith(data)
+        return@intercept
+      }
+
+      val reqBodyString = data.readUTF8Line()
+      if (reqBodyString == null) {
+        proceedWith(data)
+        return@intercept
+      }
+
+      val encodedPubsubMessage = mapper.readValue<PubsubMessage<T>>(reqBodyString)
+      proceedWith(encodedPubsubMessage)
+
+    }
+    return plugin
+  }
+}
+
